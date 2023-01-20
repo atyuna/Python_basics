faf91db5bde5f02d4f97b442ab9c905b347d7be3
atyuna <121321846+atyuna@users.noreply.github.com>
2023-01-20 11:10:16 -0500

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,11 @@
+# for loop
+
+colors = ["blue", "green", "red", "orange", "yellow"]
+
+
+for color in colors:
+    print("Do you like {} color ?".format(color))
+
+
+for number in range(1,11):
+    print("Number {}".format(number))
