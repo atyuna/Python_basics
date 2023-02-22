522c28d0f00088686c6fafd7b10cd9239f073e08
atyuna <121321846+atyuna@users.noreply.github.com>
2023-02-22 18:57:35 -0500

    Update conditions.py
    added nested if - else

--- conditions.py
+++ conditions.py
@@ -15,3 +15,24 @@ elif my_choise in shows:
     print("Your choise is a show")
 else:
     print("Your choise is not valid")
+
+    
+#--------- Nested if - else
+
+
+theater = "Lumbaba"
+rated_r_age_limit = 17
+
+print(f"Welcome to {theater} !!!")
+
+user_input = input("Enter your age: ")
+print(f"User input is {user_input}.")
+
+if int(user_input) >= rated_r_age_limit:
+    print("Enjoy the movie")
+else:
+    adult_present = input("Is the adult with you present? Yes or No? :")
+    if adult_present.lower() == "yes":
+        print("Enjoy the movie!")
+    else:
+        print("Sorry, You must be 17 to watch the movie")
